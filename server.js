0a90622d8b29b28355536af9b4416f02418b182b
Sepideh Attar <sepideh.att@gmail.com>
2022-05-30 12:20:48 +0200

    add new setting for websocket

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const app = require("./app");
-
+const server = require('http').createServer(app);
+const io = require('socket.io')(server,{
+    cors: {
+      credentials: true,
+      origin: "http://localhost:3000",
+    },
+  });
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 5005;
 
@@ -12,52 +18,52 @@ const PORT = process.env.PORT || 5005;
 // });
 
 
-// let users = []
+let users = []
 
 
-// const addUser = (userId, socketId) => {
-//   !users.some(user => user.userId == userId) &&
-//     users.push({ userId, socketId })
-// }
+const addUser = (userId, socketId) => {
+  !users.some(user => user.userId == userId) &&
+    users.push({ userId, socketId })
+}
 
-// const getUser = (userId) => {
-//   return users.find((user) => user.userId === userId);
-// };
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
 
-// const removeUser = (socketId) => {
-//   users = users.filter(user => user.socketId != socketId)
-// }
-
-
-// io.on("connection", (socket) => {
-
-//   console.log("a user connected.");
-
-//   socket.on("addUser", (userId) => {
-//     addUser(userId, socket.id);
-//     io.emit("getUsers", users);
-//   })
-
-//   //send and get message
-//   socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-//     const user = getUser(receiverId);
-//     io.to(user.socketId).emit("getMessage", {
-//       senderId,
-//       text,
-//     });
-//   });
+const removeUser = (socketId) => {
+  users = users.filter(user => user.socketId != socketId)
+}
 
 
+io.on("connection", (socket) => {
 
-//   socket.on("disconnect", () => {
-//     console.log("a user disconnected");
-//     removeUser(socket.id);
-//     io.emit("getUsers", users);
-//   })
+  console.log("a user connected.");
 
-// })
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  })
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
 
 
-app.listen(PORT, () => {
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  })
+
+})
+
+
+server.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
